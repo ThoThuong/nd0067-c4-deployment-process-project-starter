15ad9bdb9abebb63160c999e25480b791e9e5fa2
ThoThuong <tngoc5912ngocthuong37@gmail.com>
2022-12-27 02:31:14 +0700

    re-run

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,5 @@
-aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-524498553718/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-524498553718/
+# aws s3 cp --recursive --acl public-read ./www s3://elasticbeanstalk-us-east-1-524498553718/
+# aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://elasticbeanstalk-us-east-1-524498553718/
+
+aws s3 cp --recursive --acl public-read ./www s3://arn:aws:s3:::thuongtn-udacity-nano/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://arn:aws:s3:::thuongtn-udacity-nano/
