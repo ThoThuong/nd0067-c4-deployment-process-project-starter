7d086f8a5c61152e051bd3a6aaffdb3c959a6b68
ThoThuong <tngoc5912ngocthuong37@gmail.com>
2022-12-27 01:01:52 +0700

    setup circle pipeline

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+aws s3 cp --recursive --acl public-read ./www s3://arn:aws:s3:::thuongtn-udacity-nano/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://arn:aws:s3:::thuongtn-udacity-nano/
