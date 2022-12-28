6ba916bef8123e4ecefe41fae91ed7108555a1c8
ThoThuong <tngoc5912ngocthuong37@gmail.com>
2022-12-28 10:21:03 +0700

    re-run

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Thuongtn29
+export POSTGRES_PASSWORD=thuongtn3udacity
 export POSTGRES_HOST=postgres.cfjjvm1o2xaj.ap-southeast-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 # export AWS_BUCKET=thuongtn3-udacity-nano
